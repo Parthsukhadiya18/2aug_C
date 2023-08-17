ead30d7349d1e95b6fe806361d4db92fdcb9a876
Parthsukhadiya18 <parthsir18@gmail.com>
2023-08-17 11:44:21 +0530

    s

---
+++ c/01_mydetail.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    printf("Name:Parth \t\n");
+    printf("City:Surat \n");
+    printf("Dob:18/09/1998 \n");
+    printf("\'parth\' is \"parth\" \\god person");
+    return 0;
+}
